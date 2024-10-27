2e07284b685947243efb08166b22719f50cad45e
rohitnbt <rohitsinghnbt@gmail.com>
2024-10-27 17:08:41 +0530

    header fix

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -65,19 +65,19 @@ const Header = () => {
           }`}
         ></span>
       </button>) : ''}
-                <nav className={`flex flex-col md:flex-row gap-5 md:items-center justify-between bg-white ${isMobile ? 'fixed h-[calc(100svh-67px)] w-[200] transition-all duration-300 delay-100 top-[67px] translate-x-full right-0 -z-10 p-4' : ''} ${isNavOpen ? 'translate-x-0' : ''}`}>
+                <nav className={`flex flex-col md:flex-row gap-5 md:items-center justify-between bg-white ${isMobile ? 'fixed h-[calc(100svh-67px)] w-[200] transition-all duration-300 delay-100 top-[67px] translate-x-full right-0 z-10 p-4' : ''} ${isNavOpen ? 'translate-x-0' : ''}`}>
                     <ul className={`flex flex-col md:flex-row gap-5 md:items-center relative`}>
                         <li><Link className="text-[14.5px]" to="/find-suppliers">Find Suppliers</Link></li>
-                        <li><span className="text-[14.5px] flex gap-2 cursor-pointer h-[20px]" ref={boxRef} onClick={() => setOpen(!open)} onBlur={() => setOpen(false)}>Find Service Tags <img src={DropdownIcon} className={`shrink-0 transition-all duration-300 ${open ? '-rotate-180' : ''}`} alt="drop-icon" /></span></li>
+                       <div ref={boxRef}><li><span className="text-[14.5px] flex gap-2 cursor-pointer h-[20px]" onClick={() => setOpen(!open)}>Find Service Tags <img src={DropdownIcon} className={`shrink-0 transition-all duration-300 ${open ? '-rotate-180' : ''}`} alt="drop-icon" /></span></li>
                         {
-                            open ? (<ul className='md:absolute right-0 top-8 bg-white p-4 md:drop-shadow-md border md:shadow-2xl md:w-40 rounded-md flex flex-col gap-2'>
+                            open ? (<ul className='md:absolute right-0 top-8 bg-white p-4 md:drop-shadow-md border md:shadow-2xl md:w-40 rounded-md flex flex-col mt-3 gap-2'>
                                 <li><Link className="text-[14.5px] text-black hover:text-primary transition-all duration-300 delay-75 font-m" to="/option-1">Option 1</Link></li>
                                 <li><Link className="text-[14.5px] text-black hover:text-primary transition-all duration-300 delay-75 font-m" to="/option-2">Option 2</Link></li>
                                 <li><Link className="text-[14.5px] text-black hover:text-primary transition-all duration-300 delay-75 font-m" to="/option-3">Option 3</Link></li>
                                 <li><Link className="text-[14.5px] text-black hover:text-primary transition-all duration-300 delay-75 font-m" to="/option-4">Option 4</Link></li>
                                 <li><Link className="text-[14.5px] text-black hover:text-primary transition-all duration-300 delay-75 font-m" to="/option-5">Option 5</Link></li>
                             </ul>) : ''
-                        }
+                        }</div>
                     </ul>
                     <button className='text-primary py-4 px-8 border border-primary text-[15px] font-bold rounded-md font-inter'>Login / Sign Up</button>
                 </nav>

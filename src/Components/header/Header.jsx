2d3f63eea3a12d8140b211e943903a04adb9f664
rohitnbt <rohitsinghnbt@gmail.com>
2024-10-27 17:31:01 +0530

    header fix

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -7,7 +7,8 @@ const Header = () => {
     const [open, setOpen] = useState(false);
     const [isNavOpen, setIsNavOpen] = useState(false);
     const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-    const boxRef = useRef(null);
+    const DropdownRef = useRef(null);
+    const navRef = useRef(null);
 
     useLayoutEffect(() => {
         const handleResize = () => {
@@ -25,7 +26,10 @@ const Header = () => {
 
     useEffect(() => {
         function handleClickOutside(event) {
-          if (boxRef.current && !boxRef.current.contains(event.target)) {
+          if (navRef.current && !navRef.current.contains(event.target)) {
+            setIsNavOpen(false);
+          }
+          if (DropdownRef.current && !DropdownRef.current.contains(event.target)) {
             setOpen(false);
           }
         }
@@ -36,7 +40,7 @@ const Header = () => {
         };
       }, []);
   return (
-    <header className='sticky top-0 bg-white z-10'>
+    <header ref={navRef} className='sticky top-0 bg-white z-10'>
         <div className="container mx-auto p-4 bg-white">
             <div className="flex justify-between items-center">
                 <div className="logo w-[181px]">
@@ -65,10 +69,10 @@ const Header = () => {
           }`}
         ></span>
       </button>) : ''}
-                <nav className={`flex flex-col md:flex-row gap-5 md:items-center justify-between bg-white ${isMobile ? 'fixed h-[calc(100svh-67px)] w-[200] transition-all duration-300 delay-100 top-[67px] translate-x-full right-0 z-10 p-4' : ''} ${isNavOpen ? 'translate-x-0' : ''}`}>
+                <nav className={`flex flex-col md:flex-row gap-5 md:items-center justify-between bg-white ${isMobile ? 'fixed h-[calc(100svh-67px)] w-[250px] duration-300 delay-100 top-[67px] translate-x-full right-0 z-10 p-4' : ''} ${isNavOpen ? 'translate-x-0' : 'transition-all'}`}>
                     <ul className={`flex flex-col md:flex-row gap-5 md:items-center relative`}>
                         <li><Link className="text-[14.5px]" to="/find-suppliers">Find Suppliers</Link></li>
-                       <div ref={boxRef}><li><span className="text-[14.5px] flex gap-2 cursor-pointer h-[20px]" onClick={() => setOpen(!open)}>Find Service Tags <img src={DropdownIcon} className={`shrink-0 transition-all duration-300 ${open ? '-rotate-180' : ''}`} alt="drop-icon" /></span></li>
+                       <div ref={DropdownRef}><li><span className="text-[14.5px] flex gap-2 cursor-pointer h-[20px]" onClick={() => setOpen(!open)}>Find Service Tags <img src={DropdownIcon} className={`shrink-0 transition-all duration-300 ${open ? '-rotate-180' : ''}`} alt="drop-icon" /></span></li>
                         {
                             open ? (<ul className='md:absolute right-0 top-8 bg-white p-4 md:drop-shadow-md border md:shadow-2xl md:w-40 rounded-md flex flex-col mt-3 gap-2'>
                                 <li><Link className="text-[14.5px] text-black hover:text-primary transition-all duration-300 delay-75 font-m" to="/option-1">Option 1</Link></li>
